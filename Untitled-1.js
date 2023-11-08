0d0db68e2d130d8a50ff61ad1951f8a0e53d1861
Kelly Kubicek <kelly.kubicek0@gmail.com>
2023-11-08 10:43:11 +0100

    ajout des pages site photographe

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+let fruits = ["banane", "abricot", "peche", "pomme", "fraise"];
+let counter = 0;
+while (counter < fruits .lengh) {
+    if (fruits[counter] == "peche") {
+        console.log("COURGETTE") ;
+    }
+    console.log(fruits[counter]);
+    counter++;
+}
+console.log(fruits[3]);
